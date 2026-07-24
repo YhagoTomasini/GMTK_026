c84e0f514f4f755a9a7d5ccf680dde37658eaadc
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-07-23 23:46:55 -0300

    anim update

---
+++ gmtk-026/Scripts/shadow_1.gd
@@ -0,0 +1,12 @@
+extends Sprite3D
+
+@export var mCharacter : CharacterBody3D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x = mCharacter.position.x + 0.05
+	position.z = mCharacter.position.z + 0.2
