b87fece8ead5d2687f020c7a7121d2b203eac252
Yhago Tomasini <yhagooliveira20@gmail.com>
2026-07-24 13:01:30 -0300

    lookatcamera

--- gmtk-026/Scripts/Look_at_camera.gd
+++ gmtk-026/Scripts/Look_at_camera.gd
@@ -1,6 +1,17 @@
 extends AnimatedSprite3D
 
-func _process(_delta: float) -> void:
-	var cam := get_viewport().get_camera_3d()
-	if cam:
-		look_at(cam.global_position, Vector3.UP)
+func _ready() -> void:
+	pass
+	if get_parent().name == "player":
+		var cam := get_viewport().get_camera_3d()
+		if cam:
+			look_at(cam.global_position, Vector3.UP)
+			Globals.playerRotation = rotation
+	else:
+		await get_tree().create_timer(0.1).timeout
+		rotation = Globals.playerRotation
+		
+#func _process(_delta: float) -> void:
+	#var cam := get_viewport().get_camera_3d()
+	#if cam:
+		#look_at(cam.global_position, Vector3.UP)
