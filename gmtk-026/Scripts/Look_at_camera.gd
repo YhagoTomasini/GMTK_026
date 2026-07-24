adf13e859cc49986503b268c4701cf82c6e0067d
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-07-24 00:06:50 -0300

    sla

--- gmtk-026/Scripts/Look_at_camera.gd
+++ gmtk-026/Scripts/Look_at_camera.gd
@@ -1,4 +1,6 @@
 extends AnimatedSprite3D
 
-func _ready() -> void:
-	pass
+func _process(_delta: float) -> void:
+	var cam := get_viewport().get_camera_3d()
+	if cam:
+		look_at(cam.global_position, Vector3.UP)
