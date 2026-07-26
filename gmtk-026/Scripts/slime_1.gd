66db59edf8a84226066c3d11b59a2446821df94a
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-07-26 19:54:16 -0300

    slime anim dano

--- gmtk-026/Scripts/slime_1.gd
+++ gmtk-026/Scripts/slime_1.gd
@@ -14,14 +14,18 @@ extends CharacterBody3D
 @onready var hit_box: Area3D = $hit_box
 @onready var hitbox_colision: CollisionShape3D = $hit_box/hitbox_colision
 
-var is_attacking: bool = false
-var following : bool = true
+enum states {
+	ATTACKING,
+	FOLLOWING,
+	HURTING
+}
+var state := states.FOLLOWING 
 
 func _ready() -> void:
 	anim.frame = randi_range(0, 3)
 	
 func _physics_process(_delta: float) -> void:
-	if !following:
+	if state != states.FOLLOWING:
 		return
 	
 	var current_location = global_transform.origin
@@ -35,31 +39,33 @@ func _physics_process(_delta: float) -> void:
 	elif global_position.x > next_location.x:
 		anim.sorting_offset = 4
 	
-	if velocity.x < 0:
+	if velocity.x > 0:
 		anim.flip_h = true
-	elif velocity.x > 0:
+	elif velocity.x < 0:
 		anim.flip_h = false
 	
 	target_in_range()
 
 func update_target_location(target_location):
-	if following:
+	if state == states.FOLLOWING:
 		nav_agent.target_position = target_location
 	
 func takeDamage(pPosi : Vector3, damage : float) -> void:
 	life_enemy -= damage
-	following = false
-	collision_mask = 24
+	state = states.HURTING
 	anim.modulate = Color(2.5, 0.0, 0.0, 1.0)
 	knockback(pPosi)
 	if life_enemy > 0:
-		collision_mask = 3
-		await get_tree().create_timer(.8).timeout
+		anim.play("hurting")
+		
+		await anim.animation_finished
 # retirando o following o bixo n para de correr atrás de você entretanto ele n te persegue depois de morto
-		following = true
+		state = states.FOLLOWING
 		anim.modulate = Color(2.5, 2.5, 2.5, 1.0)
-		#animação de hurt <-----------
+		anim.play("default")
+		
 	else:
+		collision_mask = 24
 		anim.play("dying")
 		await anim.animation_finished
 		Globals.temp_left += time_gain
@@ -74,25 +80,25 @@ func knockback(pPosi : Vector3):
 	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	
 func target_in_range() -> void:
-	if is_attacking:
+	if state == states.ATTACKING:
 		return 
 	var target_pos = nav_agent.target_position
 	if global_position.distance_to(target_pos) < atack_range:
 		execute_attack()
 
 func execute_attack() -> void:
-	is_attacking = true
+	state = states.ATTACKING
 	hitbox_colision.disabled = false
 	await get_tree().create_timer(0.3).timeout
 	hitbox_colision.disabled = true
 	await get_tree().create_timer(0.8).timeout
-	is_attacking = false
+	state = states.FOLLOWING
 
 func _on_nav_agent_target_reached() -> void:
 	pass # Replace with function body.
 
 func _on_nav_agent_velocity_computed(safe_velocity: Vector3) -> void:
-	if !following:
+	if state != states.FOLLOWING:
 		return
 		
 	velocity = velocity.move_toward(safe_velocity, 0.25)
