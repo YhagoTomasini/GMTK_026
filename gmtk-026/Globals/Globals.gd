54d3dfc6ff31bd892eee22df3732d981e070e1d9
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-07-26 13:38:02 -0300

    telkas

--- gmtk-026/Globals/Globals.gd
+++ gmtk-026/Globals/Globals.gd
@@ -2,15 +2,11 @@ extends Node
 
 var playerRotation : Vector3
 var temp_left = 666
-var life_player: float = 20
+var life_player: int = 20
 
 func _ready() -> void:
 	pass
-
-func _process(delta: float) -> void:
-	regeneration()
 	
-
 func regeneration():
 	if life_player < 20:
 		life_player += 1
--- gmtk-026/Scripts/fire_timer.gd
+++ gmtk-026/Scripts/fire_timer.gd
@@ -2,7 +2,10 @@ extends Control
 
 @export var timer: Timer
 @export var text_number: Label
+@export var text_life : Label
+
 @export var flame_bar: Sprite2D
+@export var life_bar : Sprite2D
 
 
 func _ready() -> void:
@@ -14,11 +17,14 @@ func _process(_delta: float) -> void:
 	text_atualization()
 	if Input.is_action_just_pressed("ui_accept"):
 		Globals.temp_left += 2
-		
+	
+	
+	life_bar.scale.x = (Globals.life_player / 20)*190
 	var shader = flame_bar.material as ShaderMaterial
 	shader.set_shader_parameter("size", 2.25 - (Globals.temp_left / 666.0))
 
 func text_atualization():
+	text_life.text = str(Globals.life_player)
 	text_number.text = str(Globals.temp_left)
 
 func _on_timer_timeout() -> void:
