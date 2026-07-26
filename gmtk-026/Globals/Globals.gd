5ac3ccb5a4bb7b1b4ed519e541535ec708b4604e
Thaymoz <artemiorabello123@gmail.com>
2026-07-26 14:27:23 -0300

    Teclas e reiniciando valores

--- gmtk-026/Globals/Globals.gd
+++ gmtk-026/Globals/Globals.gd
@@ -4,6 +4,9 @@ var playerRotation : Vector3
 var temp_left = 666
 var life_player: int = 20
 
+var save_temp_left = 666
+var save_life_player = 20
+
 func _ready() -> void:
 	pass
 	
--- gmtk-026/Scripts/main.gd
+++ gmtk-026/Scripts/main.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 @onready var player: CharacterBody3D = $player
 
+func _ready() -> void:
+	Globals.life_player = Globals.save_life_player
+	Globals.temp_left = Globals.save_temp_left
 
 func _physics_process(_delta: float) -> void:
 	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
