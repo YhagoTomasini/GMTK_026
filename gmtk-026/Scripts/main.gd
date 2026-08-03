4b00e1d83f454192e4733229be47338e3ee4b527
Yhago Tomasini <yhagooliveira20@gmail.com>
2026-08-03 13:09:55 -0300

    son1teste

--- gmtk-026/Scripts/main.gd
+++ gmtk-026/Scripts/main.gd
@@ -3,6 +3,7 @@ extends Node3D
 @onready var player: CharacterBody3D = $player
 
 func _ready() -> void:
+	AudioManager.criar_aud(SoundEffect.TIPO_DE_SOM.TEMA1)
 	Globals.life_player = Globals.save_life_player
 	Globals.temp_left = Globals.save_temp_left
 
